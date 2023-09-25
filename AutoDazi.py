02a033873220e61bf75dd39a6491e3b941d8e182
Guotao He <guotao.he.0000@outlook.com>
2023-09-25 21:02:41 +0800

    Updtae

--- AutoDazi.py
+++ AutoDazi.py
@@ -11,42 +11,61 @@ import time
 import random
 import logging
 
+# 参数获取
 with open("./conf.yaml", encoding="utf8") as f:
     conf = yaml.safe_load(f)
     print(conf)
-    BackPro = conf["BackPro"]
-    TgWpm = conf["TgWpm"]
-    WpmChange = conf["WpmChange"]
-    normalDelay = conf["normalDelay"]
-    speedUp = conf["speedUp"]
-    speedDown = conf["speedDown"]
-    TgChangeTime = conf["TgChangeTime"]
-    TimeChange = conf["TimeChange"]
-    MaxLines = conf["MaxLines"]
-    WD = conf["WebDriver"]
-    WaitTime = conf["WaitTime"]
+    back_pro = conf["back_pro"]
+    target_wpm = conf["target_wpm"]
+    wpm_change = conf["wpm_change"]
+    normal_delay = conf["normal_delay"]
+    speed_up = conf["speed_up"]
+    speed_down = conf["speed_down"]
+    target_change_time = conf["target_change_time"]
+    time_change = conf["time_change"]
+    max_lines = conf["max_lines"]
+    webdriver_setting = conf["webdriver_setting"]
+    wait_time = conf["wait_time"]
 
-if WD == "Edge":
+if webdriver_setting == "Edge":
     driver = webdriver.Edge()
-elif WD == "Firefox":
+elif webdriver_setting == "Firefox":
     driver = webdriver.Firefox()
-elif WD == "Chrome":
+elif webdriver_setting == "Chrome":
     driver = webdriver.Chrome()
 else:
     logging.error("Driver Name? (Edge or Firefox or Chrome)")
 
+start_flag = 0
+def CallBackStartFunc():
+    global start_flag
+    start_flag = 1
+
+exit_flag = 0
+def CallBackExitFunc():
+    global exit_flag
+    exit_flag = 1
+
+keyboard.add_hotkey("alt+shift+f1",CallBackStartFunc)
+keyboard.add_hotkey("alt+shift+esc",CallBackExitFunc)
 
 logging.warning("Start......")
 driver.get('https://dazi.kukuw.com/')
-logging.warning("Wait " + str(WaitTime) + "s")
-time.sleep(WaitTime)
+
+logging.warning("Alt+Shift+F1 to Start......")
+
+while start_flag == 0:
+    time.sleep(1)
+
+
+# 获取文章所有文字
 logging.warning("Get Article......")
 
 page_source =  driver.page_source
 list = []
 start = 0
 # 最大300行
-for id in range(MaxLines):
+for id in range(max_lines):
     div_s = page_source.find("<div id=\"i_"+str(id)+"\"",start)
     if div_s == -1:
         break
@@ -57,41 +76,45 @@ for id in range(MaxLines):
     start = wordEnd
     list.append(word)
 
-exit_flag = 0
-def callbackfunc():
-    global exit_flag
-    exit_flag = 1
+print(len(list))
+print(list)
 
-keyboard.add_hotkey("alt+shift+esc",callbackfunc)
+# 开始打字
 
 st = time.time()
 lt = time.time()
-wpm = TgWpm
-for numLines in range(len(list)):
-    logging.warning("line %d",numLines)
-    line = list[numLines]
+wpm = target_wpm
+
+for num_lines in range(len(list)):
+    logging.warning("line %d", num_lines)
+    line = list[num_lines]
+    print(line)
     for word in line:
         if exit_flag == 1:
             break
-        if time.time() - st > (TgChangeTime+TimeChange*np.random.random()-(TimeChange/2)):
+        # 速度控制
+        if time.time() - st > (target_change_time + 2*time_change*np.random.random()-time_change):
             wpm_random = np.random.normal(0,1)
             if wpm_random < 0:
-                wpm = TgWpm + speedDown * wpm_random
+                wpm = target_wpm + speed_down * wpm_random
             else:
-                wpm = TgWpm + speedUp * wpm_random
-            print("New TgWPM: " + str(wpm) + " | Practical WPM: " + str(exWPM))
+                wpm = target_wpm + speed_up * wpm_random
+            print("New TgWPM: " + str(wpm) + " | Practical WPM: " + str(practical_wpm))
             st = time.time()
-        time.sleep(60/(wpm+WpmChange*np.random.random()-(WpmChange/2)))
+        time.sleep(60/(wpm+2*wpm_change*np.random.random()-wpm_change))
         driver.switch_to.active_element.send_keys(word)
-        exWPM = 60/(time.time()-lt)
+        # 信息显示
+        practical_wpm = 60/(time.time() - lt)
         lt = time.time()
-        if random.uniform(0,100) <= BackPro and word != line[len(line)-1]:
-            time.sleep(normalDelay)
+        # 随机退格
+        if random.uniform(0,100) <= back_pro and word != line[len(line)-1]:
+            time.sleep(normal_delay)
             driver.switch_to.active_element.send_keys(Keys.BACKSPACE)
-            time.sleep(normalDelay)
+            time.sleep(normal_delay)
             driver.switch_to.active_element.send_keys(word)
     if exit_flag == 1:
         break
+
 logging.warning("Finish!")
 time.sleep(1)
 input("Exit?")
